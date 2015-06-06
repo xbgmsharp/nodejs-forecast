1b80f4f1511e7695b8941d4210ba2133ae7ffd95
root <xbgmsharp@gmail.com>
2015-06-06 14:22:11 +0000

    Improve OpenShift gestion

--- app.js
+++ app.js
@@ -11,14 +11,15 @@ var mongoexpress = require('mongo-express');
 // MongoDB driver
 var mongo = require('mongodb');
 var monk = require('monk');
+// Default to a 'localhost' configuration:
+var connection_string = 'localhost:27017/forecast';
+// if OPENSHIFT env variables are present, use the available connection info:
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
 	connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
 		process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
 		process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
 		process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
 		process.env.OPENSHIFT_APP_NAME;
-} else {
-	connection_string = 'localhost:27017/forecast';
 }
 var db = monk(connection_string);
 
