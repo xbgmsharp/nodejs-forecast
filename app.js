f962e9813ca8803b3339714958e5368678b52aa0
root <xbgmsharp@gmail.com>
2015-06-08 14:55:07 +0000

    Fix MongoDB auth using OpenShift http://stackoverflow.com/questions/30331800/auth-couldnt-find-user-mongodbadmin

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
 		process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
 		process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
 		process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-		process.env.OPENSHIFT_APP_NAME;
+		process.env.OPENSHIFT_APP_NAME + "?authSource=admin";
 }
 var db = monk(connection_string);
 
