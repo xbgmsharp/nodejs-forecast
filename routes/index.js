fd497de0af3557ae294443c4a220e2050d4a34b0
root <xbgmsharp@gmail.com>
2015-12-16 14:49:50 +0000

    Update front page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
+	res.render('index', { title: 'nodejs-forecast', desc: 'A NodeJs Proxy for Forecast.io', url: 'https://github.com/xbgmsharp/nodejs-forecast' });
 });
 
 module.exports = router;
